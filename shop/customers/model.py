331ed3269b0efbe7aee4bc57becb82f5e2b71195
Дамир <mikadzyku17@icloud.com>
2022-12-01 21:32:40 +0500

    #36 create customer order table

--- shop/customers/model.py
+++ shop/customers/model.py
@@ -1,6 +1,7 @@
 from shop import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+import  json
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -20,6 +21,32 @@ class Register(db.Model, UserMixin):
     zipcode = db.Column(db.String(50), unique=False)
     profile = db.Column(db.String(200), unique=False, default='profile.jpg')
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    class JsonEcodedDict(db.TypeDecorator):
+        impl = db.Text
+
+        def process_bind_param(self, value, dialect):
+            if value is None:
+                return '{}'
+            else:
+                return json.dumps(value)
+
+        def process_result_value(self, value, dialect):
+            if value is None:
+                return {}
+            else:
+                return json.loads(value)
+
+    class CustomerOrder(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        invoice = db.Column(db.String(20), unique=True, nullable=False)
+        status = db.Column(db.String(20), default='В ожидании', nullable=False)
+        customer_id = db.Column(db.Integer, unique=False, nullable=False)
+        date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+        orders = db.Column(JsonEcodedDict)
+
+        def __repr__(self):
+            return '<CustomerOrder %r>' % self.invoice
 
     def __repr__(self):
         return '<Register %r>' % self.name
