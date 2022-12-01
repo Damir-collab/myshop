6ae870267c39120384fc8e85079f8fdfd5d801ee
Дамир <mikadzyku17@icloud.com>
2022-12-01 21:10:43 +0500

    #34 Кнопки регистрация и вход на панели

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -38,3 +38,8 @@ def customerLogin():
         flash('Вы неправильно ввели логин или пароль', 'danger')
         return redirect(url_for('customerLogin'))
     return render_template('customer/login.html', form=form)
+
+@app.route('/customer/logout')
+def customer_logout():
+    logout_user()
+    return redirect(url_for('home'))
